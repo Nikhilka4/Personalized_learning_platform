407b22fb9e49aac48e152be3721df50c69120e80
Nikhilka4 <106381060+Nikhilka4@users.noreply.github.com>
2024-03-02 18:08:13 +0530

    d

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -13,7 +13,7 @@ import {
   sendToken,
 } from "../utils/jwt";
 import { redis } from "../utils/redis";
-import { decode } from "punycode";
+import cloudinary from "cloudinary"
 import { getUserById } from "../services/user.service";
 
 // Registering the user
@@ -324,3 +324,107 @@ export const updateUserInfo = CatchAsyncError(
     }
   }
 );
+
+
+// update user password
+interface IUpdatePassword {
+  oldPassword: string;
+  newPassword: string;
+}
+
+export const updatePassword = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { oldPassword, newPassword } = req.body as IUpdatePassword;
+
+    if (!oldPassword ||!newPassword) {
+      return next(new ErrorHandler("Please enter old password and new password", 400));
+    }
+
+    const user = await userModel.findById(req.user?._id).select("+password");
+
+    if (user?.password === undefined) {
+      return next(new ErrorHandler("Invalid user", 400));
+    }
+
+    const isPasswordMatch = await user?.comparePassword(oldPassword);
+
+    if (!isPasswordMatch) {
+      return next(new ErrorHandler("Invalid old password", 400));
+    }
+
+    user.password = newPassword;
+
+    await user.save();
+
+    await redis.set(req.user?._id, JSON.stringify(user));
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+})
+
+
+// update profile picture
+interface IUpdateProfilePicture {
+   avatar: string
+}
+
+
+
+export const updateProfilePicture = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    
+    const {avatar} = req.body;
+
+    const userId =  req.user?._id;
+
+    const user = await userModel.findById(userId);
+
+    if (avatar && user){
+
+      // if we already have an avatar than call this if
+      if(user?.avatar?.public_id){
+
+        // first delete the old image
+        await cloudinary.v2.uploader.destroy(user?.avatar?.public_id);
+
+        const myCloud = await cloudinary.v2.uploader.upload(avatar,{
+          folder: "avatars",
+          width: 150,
+        });
+        user.avatar = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        }
+
+      } else{
+        const myCloud = await cloudinary.v2.uploader.upload(avatar,{
+          folder: "avatars",
+          width: 150,
+        });
+        user.avatar = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        }
+      }
+    }
+
+    await user?.save();
+
+    await redis.set(userId, JSON.stringify(user));
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+
+
+  } catch (error: any) {
+    return next(new ErrorHandler(error.message, 400));  
+  }
+})
